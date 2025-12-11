f93482d2f26e6ed89214b518319d05360b5baedc
Akshit4340 <24ce01005@iitbbs.ac.in>
2025-12-11 19:33:43 +0530

    initialize the project

---
+++ project/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import config from '@/config';
+
+const app = express();
+
+app.listen(config.PORT, () => {
+  console.log(`Server is running on http://localhost:${config.PORT}`);
+});
